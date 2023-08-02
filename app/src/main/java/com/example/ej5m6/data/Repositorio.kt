008629dbc8a6e5f116a93780757b5e8d2ed4da68
ginger cea <gingercea@gmail.com>
2023-08-02 19:29:26 -0400

    se creo  los 3 componentes terreno api retrofit y repositorio

--- app/src/main/java/com/example/ej5m6/data/Repositorio.kt
+++ app/src/main/java/com/example/ej5m6/data/Repositorio.kt
@@ -1,10 +1,13 @@
 package com.example.ej5m6.data
 
+import com.example.ej5m6.data.remote.Terreno
 import com.example.ej5m6.data.remote.TerrenoAPI
 
 class Repositorio( private val terrenoAPI: TerrenoAPI) {
 
-   // suspend fun cargarTerreno():
+     suspend fun cargarTerreno():List<Terreno>{
+      return.terrenoAPI.getData()
+     }
 
 
 }
