b90343da49eb855a1799ae78bee22f62dd724123
ginger cea <gingercea@gmail.com>
2023-08-01 22:21:30 -0400

    repositorio implementacion

---
+++ app/src/main/java/com/example/ej5m6/data/Repositorio.kt
@@ -0,0 +1,10 @@
+package com.example.ej5m6.data
+
+import com.example.ej5m6.data.remote.TerrenoAPI
+
+class Repositorio( private val terrenoAPI: TerrenoAPI) {
+
+   // suspend fun cargarTerreno():
+
+
+}
--- app/src/main/java/com/example/ej5m6/data/remote/TerrenoAPI.kt
+++ app/src/main/java/com/example/ej5m6/data/remote/TerrenoAPI.kt
@@ -4,6 +4,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface TerrenoAPI {
+
     @GET("realestate")
   suspend  fun getData(): Response<List<Terreno>>
 
