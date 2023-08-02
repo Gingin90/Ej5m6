19d2626490b737b33bb606f8690f19f1efefe3a1
ginger cea <gingercea@gmail.com>
2023-08-01 21:56:17 -0400

    se crearon los 3 componentes terreno terreno api y retrofit

---
+++ app/src/main/java/com/example/ej5m6/data/remote/TerrenoAPI.kt
@@ -0,0 +1,13 @@
+package com.example.ej5m6.data.remote
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface TerrenoAPI {
+    @GET("realestate")
+  suspend  fun getData(): Response<List<Terreno>>
+
+
+
+
+}
