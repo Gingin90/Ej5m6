bd799d66966d0cbfc00ae58179bff4802a1bfcba
ginger cea <gingercea@gmail.com>
2023-08-04 20:20:06 -0400

    Ej7m6 se creo el tv id en detalle fragment con binding, y listener en adapter mas layout de detalle fragment

--- app/src/main/java/com/example/ej5m6/presentation/Adapter.kt
+++ app/src/main/java/com/example/ej5m6/presentation/Adapter.kt
@@ -1,9 +1,13 @@
 package com.example.ej5m6.presentation
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.ej5m6.R
 import com.example.ej5m6.data.local.TerrenoEntity
 import com.example.ej5m6.databinding.ItemTerrenoBinding
 
@@ -15,8 +19,7 @@ private val listItemTerreno = mutableListOf<TerrenoEntity>()
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemTerrenoViewHolder {
-
-    binding = ItemTerrenoBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        binding = ItemTerrenoBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return ItemTerrenoViewHolder(binding)
     }
 
@@ -36,6 +39,14 @@ private val listItemTerreno = mutableListOf<TerrenoEntity>()
     class ItemTerrenoViewHolder(val v:ItemTerrenoBinding):RecyclerView.ViewHolder(v.root) {
         fun bind(terreno: TerrenoEntity) {
             v.imgTerreno.load(terreno.img)
+            v.imgTerreno.setOnClickListener{
+                val bundle = Bundle()
+                bundle.putString("id",terreno.id)
+                bundle.putString("imagen", terreno.img)
+                bundle.putString("precio",terreno.price.toString())
+                Navigation.findNavController(v.root).navigate(R.id.action_listadoTerreno_to_detalleFragment)
+
+            }
 
         }
 
